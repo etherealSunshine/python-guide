506e861f56548d146811fc08209d5d15bad3f99e
Utkarsh Singh <81072340+etherealSunshine@users.noreply.github.com>
2021-08-03 15:40:32 +0530

    Add files via upload
    Talks about variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+a = 2
+
+# this is a variable with the value of a integer
+a = " abv"
+# this is a varible with the value of a string
+a = True
+# this is a variable with the Value True
+
+a = 0.234
+# this is a variable with the value of a float
+
+# you can check what type a varible is by using the type() command
+print(type(a))
+# variables are also case sensitive, that is a != A
+
+# variables cannot start with a number, for eg if i try this
+2a = 5
+print(2a)
+# error invalid syntax should show up
+# this is also the case for symbols
+# it is advised to use snake case while nameing variables. this means
+panda_ml_1 = 0
+# is preferred compared to this
+PandaMl1 - 0
